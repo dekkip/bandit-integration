f8f4d626dbcfbd9050f48c0b1b6f926c1283e82e
dekkip <130106517+dekkip@users.noreply.github.com>
2024-09-18 22:01:54 -0400

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -1,14 +1,10 @@
-import os
-
 def insecure_function():
     # Hardcoded password (Bandit will flag this)
     password = "supersecretpassword"
     print(f"The password is: {password}")
 
 def main():
-    # Use of os.system (Bandit will flag this)
-    os.system('echo Hello, World!')
-    print("Good morning, World!")
+    print("Hello World!")
 
 if __name__ == "__main__":
     insecure_function()
